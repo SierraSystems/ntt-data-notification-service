2dc908963af52ea74ff5e09703da7a73dcd4cf4f
TayGov <Taylor.Clausen@gov.bc.ca>
2020-05-11 14:10:49 -0700

    Initial Commit

---
+++ src/main/java/ca/bc/gov/splunknotificationservice/SplunkNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.splunknotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplunkNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplunkNotificationServiceApplication.class, args);
+	}
+
+}
