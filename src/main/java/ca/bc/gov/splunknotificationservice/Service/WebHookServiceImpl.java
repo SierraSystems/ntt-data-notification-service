7f44d8f5255e21d0520f0305f86448ba5b861e29
TayGov <Taylor.Clausen@gov.bc.ca>
2020-05-12 11:19:34 -0700

    Adding enabling properties
    Template rocket message

--- src/main/java/ca/bc/gov/splunknotificationservice/Service/WebHookServiceImpl.java
+++ src/main/java/ca/bc/gov/splunknotificationservice/Service/WebHookServiceImpl.java
@@ -22,11 +22,22 @@ public class WebHookServiceImpl implements WebHookService {
     SplunkProperites splunkProperites;
     Logger logger = LoggerFactory.getLogger(WebHookServiceImpl.class);
     public ResponseEntity<String> postMessage(SplunkAlert splunkAlert) {
-        logger.info("Posting message to Teams");
-        post(splunkProperites.getTeamsUrl(), mapTeams(splunkAlert));
-        logger.info("Posting message to Rocket Chat");
-        post(splunkProperites.getRocketUrl(), mapRocket(splunkAlert));
 
+        if (splunkProperites.getTeamsEnabled()) {
+            logger.info("Posting message to Teams");
+            post(splunkProperites.getTeamsUrl(), mapTeams(splunkAlert));
+        }
+
+        if (splunkProperites.getRocketEnabled()) {
+            logger.info("Posting message to Rocket Chat");
+            post(splunkProperites.getRocketUrl(), mapRocket(splunkAlert));
+        }
+
+        if (splunkProperites.getTeamsCardsEnabled()) {
+            logger.info("Posting card to teams");
+            //replace with map card
+            post(splunkProperites.getTeamsUrl(), mapTeams(splunkAlert));
+        }
         //TODO: Does splunk care about return?
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
@@ -43,14 +54,15 @@ public class WebHookServiceImpl implements WebHookService {
 
     private RocketMessage mapRocket(SplunkAlert splunkAlert) {
         RocketMessage  rocketMessage = new RocketMessage();
-        rocketMessage.setText(splunkAlert.getResults_link());
+        String text = String.format("%s : %s", splunkAlert.getSearch_name(), splunkAlert.getResults_link());
+        rocketMessage.setText(text);
         rocketMessage.setAlias(splunkProperites.getRocketMessageAlias());
         return rocketMessage;
     }
     private TeamsMessage mapTeams(SplunkAlert splunkAlert) {
         TeamsMessage teamsMessage = new TeamsMessage();
         teamsMessage.setText(splunkAlert.getResults_link());
-        teamsMessage.setSummary(splunkProperites.getTeamsMessageSummary());
+        teamsMessage.setSummary(splunkAlert.getSearch_name());
         return teamsMessage;
     }
 }
