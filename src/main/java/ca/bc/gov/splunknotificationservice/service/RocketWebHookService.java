27df5940903ab00c55f9b487ab44721aa8b47db7
TayGov <Taylor.Clausen@gov.bc.ca>
2020-05-20 12:02:31 -0700

    Clean- up

--- src/main/java/ca/bc/gov/splunknotificationservice/service/RocketWebHookService.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.splunknotificationservice.service;
-
-import ca.bc.gov.splunknotificationservice.model.splunk.SplunkAlert;
-import org.springframework.http.ResponseEntity;
-
-public interface RocketWebHookService {
-
-    public ResponseEntity<String> postMessage(SplunkAlert splunkAlert, String token, String encodedRocketUrl);
-}
