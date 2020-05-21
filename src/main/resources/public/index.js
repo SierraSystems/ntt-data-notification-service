7023853b4e788ffd2962f51fee97a3343cf83610
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-05-21 16:03:09 -0700

    pull values for urls, clean up code

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,45 +1,63 @@
-function getStarted() {
-    console.log("hello there");
+// populate these two fields based on your local dev environment
+const localIP = "192.168.56.1";
+const port = "8888";
 
-    var string = `{
-        "splunkWebHookUrls": [
-          {
-          "chatApp": "TEAMS",
-          "url": "https://outlook.office.com/webhook/45ab4e59-e51b-4d89-8a40-648375f19767@65e4e06f-f263-4c1f-becb-90deb8c2d9ff/IncomingWebhook/a01363ce862e48938c923e60110953eb/757dc14b-e969-480f-9023-75aa4c2e7f15"
-          },
-          {
-          "chatApp": "ROCKET_CHAT",
-          "url": "https://chat.pathfinder.gov.bc.ca/hooks/nRaa9Pa76mREwmMp2/ggavY8ub5MthKw2teuj6yRZfBb2SGLjW8vKi9kuX5jAfLQpn"
-          }
-        ]
-    }`;
-    console.log(btoa(string).replace('+', '-').replace('/', '_').replace(/=+$/, ''));
+function generateWebHookUrlString() {
+    const teamsUrlIdentifier = ".teams-url";
+    const rocketCharUrlIdentifier = ".rocket-chat-url";
+    let webHookUrls = {};
+    let splunkWebHookUrls = [];
 
-    // try getting token from yml
-    // Get document, or throw exception on error
-//    try {
-//        const doc = jsyaml.safeLoad(fs.readFileSync('../application.yml', 'utf8'));
-//        console.log(doc);
-//    } catch (e) {
-//        console.log(e);
-//    }
-
-    //http://192.168.56.1:8888/splunk/alert/3ac241ab-4448-4f1a-b925-29ac7fe6e37e/
-
-    let webHookUrls = {
-        "splunkWebHookUrls": []
-    };
-
-    for (i = 0; i < $('.teams-url').length; i++) {
+    // loop through teams urls
+    for (i = 0; i < $(teamsUrlIdentifier).length; i++) {
         // Create object
         const chatAppUrl = {
             "chatApp": "TEAMS",
-            "url": $('.teams-url').eq(i).val()
+            "url": $(teamsUrlIdentifier).eq(i).val()
         };
 
         // Push object to the array
-        webHookUrls.splunkWebHookUrls.push(chatAppUrl);
+        splunkWebHookUrls.push(chatAppUrl);
     }
-    //print the array or use it for your further logic
-    console.log(webHookUrls);
+
+    // loop through rocket chat urls
+    for (i = 0; i < $(rocketCharUrlIdentifier).length; i++) {
+        // Create object
+        const chatAppUrl = {
+            "chatApp": "ROCKET_CHAT",
+            "url": $(rocketCharUrlIdentifier).eq(i).val()
+        };
+
+        // Push object to the array
+        splunkWebHookUrls.push(chatAppUrl);
+    }
+
+    webHookUrls.splunkWebHookUrls = splunkWebHookUrls;
+    return JSON.stringify(webHookUrls);
+}
+
+// url safe UTF-8 base 64 encoding
+function base64EncodeString(string) {
+    return btoa(string).replace('+', '-').replace('/', '_').replace(/=+$/, '');
+}
+
+function createNewTeamsUrlInput() {
+    let txtNewInputBox = document.createElement('div');
+    txtNewInputBox.innerHTML = "<input type='text' class='teams-url'><br />";
+    document.getElementById("add-teams-url-here").appendChild(txtNewInputBox);
+}
+
+function createNewRocketChatUrlInput() {
+    let txtNewInputBox = document.createElement('div');
+    txtNewInputBox.innerHTML = "<input type='text' class='rocket-chat-url'><br />";
+    document.getElementById("add-rocket-chat-url-here").appendChild(txtNewInputBox);
+}
+
+function generateUrl() {
+    // temporarily for local dev
+    const baseUrl = `http://${localIP}:${port}/splunk/alert/`;
+    const token = $(".token").val();
+    const base64EncodedUrl = base64EncodeString(generateWebHookUrlString());
+
+    return `${baseUrl}${token}/${base64EncodedUrl}`;
 }
