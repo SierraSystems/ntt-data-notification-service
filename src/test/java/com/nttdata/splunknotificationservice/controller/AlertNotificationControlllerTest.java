e0b084303ec10107aa6c4b98e7fc22711bb04bc3
TayGov <Taylor.Clausen@gov.bc.ca>
2020-05-22 09:50:03 -0700

    Adding testing

---
+++ src/test/java/com/nttdata/splunknotificationservice/controller/AlertNotificationControlllerTest.java
@@ -0,0 +1,4 @@
+package com.nttdata.splunknotificationservice.controller;
+
+public class AlertNotificationControlllerTest {
+}
---
+++ src/test/java/com/nttdata/splunknotificationservice/service/WebHookServiceImplTest.java
@@ -0,0 +1,4 @@
+package com.nttdata.splunknotificationservice.service;
+
+public class WebHookServiceImplTest {
+}
