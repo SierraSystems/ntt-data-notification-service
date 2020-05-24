78e18ebf4a68bc65b52a922d7c98cde72d07654e
alex <alex.joyeux@gov.bc.ca>
2020-05-23 20:06:58 -0700

    Refactored TeamsChannelService

--- src/main/java/com/nttdata/nttdatanotificationservice/teams/models/TeamsInput.java
+++ src/main/java/com/nttdata/nttdatanotificationservice/teams/models/TeamsInput.java
@@ -4,61 +4,72 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class TeamsInput {
+
   private String type;
   private String id;
   private Boolean isMultiline;
   private String title;
   private Boolean isMultiSelect;
-  private ArrayList<TeamsChoice> choices;
+  private ArrayList<TeamsChoice> choices = new ArrayList<>();
+
+
+  public TeamsInput(String id, String type, String title) {
+    this.type = type;
+    this.id = id;
+    this.title = title;
+  }
 
   @JsonProperty("@type")
   public String getType() {
     return type;
   }
 
-  @JsonProperty("@type")
-  public void setType(String type) {
-    this.type = type;
-  }
-
   public String getId() {
     return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
   }
 
   public Boolean getMultiline() {
     return isMultiline;
   }
 
-  public void setMultiline(Boolean multiline) {
-    isMultiline = multiline;
-  }
-
   public String getTitle() {
     return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
   }
 
   public Boolean getMultiSelect() {
     return isMultiSelect;
   }
 
-  public void setMultiSelect(Boolean multiSelect) {
-    isMultiSelect = multiSelect;
-  }
-
   public ArrayList<TeamsChoice> getChoices() {
     return choices;
   }
 
-  public void setChoices(
-      ArrayList<TeamsChoice> choices) {
-    this.choices = choices;
+  public void addChoice(TeamsChoice choice) {
+    this.choices.add(choice);
+  }
+
+  public static TeamsInput defaultTeamsInput(String id, String type, String title) {
+    return new TeamsInput(id, type, title);
+  }
+
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setMultiline(Boolean multiline) {
+    isMultiline = multiline;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setMultiSelect(Boolean multiSelect) {
+    isMultiSelect = multiSelect;
   }
 }
