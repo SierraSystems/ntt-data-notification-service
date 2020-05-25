30dbf0624fc9882f0be0700af9d07162f2bd5a57
Alan Dodge <alan.dodge@gov.bc.ca>
2020-05-25 11:48:38 -0700

    Updated RocketChannelService to allow variable result

--- src/main/java/com/nttdata/nttdatanotificationservice/rocket/models/RocketMessage.java
+++ src/main/java/com/nttdata/nttdatanotificationservice/rocket/models/RocketMessage.java
@@ -1,16 +1,24 @@
 package com.nttdata.nttdatanotificationservice.rocket.models;
 
 public class RocketMessage {
+
+    private static final String AVATAR = "https://user-images.githubusercontent.com/51387119/82707419-ddb1c600-9c30-11ea-8bfa-b3c624b23cdd.png";
+    private static final String EMOJI = "";
+
     private String alias;
     private String avatar;
     private String emoji;
     private String text;
 
+    public RocketMessage(String alias, String avatar, String emoji) {
+        this.alias = alias;
+        this.avatar = avatar;
+        this.emoji = emoji;
+    }
+
     public String getAlias() { return alias; }
 
     public void setAlias(String alias) {  this.alias = alias; }
-
-
 
     public String getAvatar() {  return avatar;  }
 
@@ -24,4 +32,7 @@ public class RocketMessage {
 
     public void setText(String text) { this.text = text; }
 
+    public static RocketMessage defaultNttMessage(String alias) {
+        return new RocketMessage(alias, AVATAR, EMOJI);
+    }
 }
